67519d5866191f9d60d3e421cf39e7153fc26cec
Kozo Nishida <knishida@riken.jp>
2011-07-15 14:27:48 +0900

    expand multiple rules

--- pybngl.py
+++ pybngl.py
@@ -7,17 +7,28 @@ from model.model import BINDING_NONE
 from model.model import BINDING_ANY
 from model.model import BINDING_UNSPECIFIED
 from model.parser import Parser
-from solver.ODESolver import ODESolver
+#from solver.ODESolver import ODESolver
 from process.process import FunctionMaker
 from Simulator import Simulator
 
-m      = Model()
+#from model import Model
+#from model import Species
+#from model import BINDING_SPECIFIED
+#from model import BINDING_NONE
+#from model import BINDING_ANY
+#from model import BINDING_UNSPECIFIED
+#from parser import Parser
+#from ODESolver import ODESolver
+#from process import FunctionMaker
+
+
+m = Model()
 parser = Parser()
 fm = FunctionMaker()
 sim = Simulator()
 
 global_list = []
-tmp_list    = []
+tmp_list = []
 
 class AnyCallable(object):
     def __init__(self, key, outer=None):
@@ -75,7 +86,6 @@ class AnyCallable(object):
             tmp_list[-1]["children"].append({"type": "bracket", "value": str(key)})
         else:
             tmp_list[-1]["children"] = [{"type": "bracket", "value": str(key)}]
-
         return self
 
     def operator(self, rhs):
@@ -84,7 +94,7 @@ class AnyCallable(object):
         tmp_dict = {}
         print rhs
 
-        tmp_dict["type"]     = rhs
+        tmp_dict["type"] = rhs
         tmp_dict["children"] = tmp_list
         global_list.append(tmp_dict)
 
@@ -135,30 +145,37 @@ class ReactionRules(object):
         pass
 
     def __exit__(self, *arg):
-        id = 0
 
-        print '\n*** reactants ***'
-        reactants = read_patterns(m, parser, global_list[id]['children'][0])
-        for i in reactants:
-            print i
 
-        print '\n*** products ***'
-        products = read_patterns(m, parser, global_list[id]['children'][1])
-        for i in products:
-            print i
+        for id, v in enumerate(global_list):
+            reactants = read_patterns(m, parser, v['children'][0])
+            products = read_patterns(m, parser, v['children'][1])
+            rule = m.add_reaction_rule(reactants, products, k_name='MassAction', k=.3)
 
-        
+            if v['type'] == 'neq':
+                tmp = products
+                products = reactants
+                reactants = tmp
+                rule = m.add_reaction_rule(reactants, products, k_name='MassAction', k=.3)
+            
+               
+#        print '\n*** reactants ***'
+#        reactants = read_patterns(m, parser, global_list[id]['children'][0])
+#        for i in reactants:
+#            print i
 
-        rule = m.add_reaction_rule(reactants, products, k_name='MassAction', k=.3)
-        print m.reaction_rules.items()
+#        print '\n*** products ***'
+#        products = read_patterns(m, parser, global_list[id]['children'][1])
+#        for i in products:
+#            print i
+
 
         N_A = 6.0221367e+23
-        sp_str_list = ['R(l~Y)', 'L(r)']
+        sp_str_list = ['egfr(l, r, Y1068~Y, Y1148~Y)', 'egf(r)', 'Sos(dom)', 'Shc(PTB, Y317~Y)', 'Grb2(SH2, SH3)']
+        seed_values = [10000. * N_A, 10000. * N_A, 10000. * N_A, 10000. * N_A, 10000. * N_A]
+
         seed_species = parser.parse_species_array(sp_str_list, m)
-        seed_values = [10000. * N_A, 10000. * N_A]
-
         results = m.generate_reaction_network(seed_species, 10)
-
 
 
         print '<< reaction rules >>'
@@ -185,49 +202,50 @@ class ReactionRules(object):
                 cnt += 1
         print ''
 
-        sp_num = len(m.concrete_species)
-
-        # Initial values for species.
-        variables = []
-        for i in range(sp_num):
-            variables.append(0.0)
-        for i, v in enumerate(seed_values):
-            variables[i] = v
-
-        global fm
-        global sim
-
-        volume = 1
-        functions = fm.make_functions(m, results, volume)
-        the_solver = ODESolver()
-        sim.initialize(the_solver, functions, variables)
-
-        step_num = 120
-        sim.step(step_num)
-
-        output_series = sim.get_logged_data()
-        header = 'time, '
-        for i, sp_id in enumerate(sorted(m.concrete_species.iterkeys())):
-            if i > 0:
-                header += ', '
-            sp = m.concrete_species[sp_id]
-            header += sp.str_simple()
-        print header
-        print output_series
-        print ''
-
-        output_terminal = output_series[step_num - 1]
-        result = str(output_terminal[0])
-        result += ': '
-        for i, v in enumerate(output_terminal):
-            if i > 1:
-                result += ', '
-            if i > 0:
-                value = v / N_A
-                result += str(value)
-
-        print header
-        print result
+#        sp_num = len(m.concrete_species)
+#
+#        # Initial values for species.
+#        variables = []
+#        for i in range(sp_num):
+#            variables.append(0.0)
+#        for i, v in enumerate(seed_values):
+#            print i, v
+#            variables[i] = v
+#
+#        global fm
+#        global sim
+#
+#        volume = 1
+#        functions = fm.make_functions(m, results, volume)
+#        the_solver = ODESolver()
+#        sim.initialize(the_solver, functions, variables)
+#
+#        step_num = 120
+#        sim.step(step_num)
+#
+#        output_series = sim.get_logged_data()
+#        header = 'time, '
+#        for i, sp_id in enumerate(sorted(m.concrete_species.iterkeys())):
+#            if i > 0:
+#                header += ', '
+#            sp = m.concrete_species[sp_id]
+#            header += sp.str_simple()
+#        print header
+#        print output_series
+#        print ''
+#
+#        output_terminal = output_series[step_num - 1]
+#        result = str(output_terminal[0])
+#        result += ': '
+#        for i, v in enumerate(output_terminal):
+#            if i > 1:
+#                result += ', '
+#            if i > 0:
+#                value = v / N_A
+#                result += str(value)
+#
+#        print header
+#        print result
 
 
 
@@ -239,8 +257,8 @@ class MoleculeTypes(object):
     def __exit__(self, *arg):
         global tmp_list
         mole_entity_list = [] # string. check for double registration such as A and A.B.
-        mole_state_list  = [] # string. check for state_type registration.
-        mole_state_dict  = {} # tuple.   key:State_type
+        mole_state_list = []  # string. check for state_type registration.
+        mole_state_dict = {}  # tuple.   key:State_type
         for i in tmp_list:
 
             if i['name'] not in mole_entity_list and i['name'] != '.':
@@ -295,12 +313,27 @@ def read_entity(sp, m, p, entity, binding_components):
                             en_comp.binding_state = BINDING_ANY
                         elif binding_type == '?':
                             en_comp.binding_state = BINDING_UNSPECIFIED
+
+#                        elif binding_type.isdigit():
+#                            en_comp.binding_state = BINDING_SPECIFIED
+#                            binding_id = int(binding_type)
+#                            if not binding_id in binding_components:
+#                                binding_components[binding_id] = []
+#                            binding_components[binding_id].append(en_comp)
+
                         elif binding_type.isdigit():
-                            en_comp.binding_state = BINDING_SPECIFIED
+                            en_comp.binding_state = BINDING_ANY
                             binding_id = int(binding_type)
                             if not binding_id in binding_components:
                                 binding_components[binding_id] = []
-                            binding_components[binding_id].append(en_comp)
+                                binding_components[binding_id].append(en_comp)
+                            else:
+                                en_comp.binding_state = BINDING_SPECIFIED
+                                tmp = binding_components[binding_id].pop()
+                                tmp.binding_state = BINDING_SPECIFIED
+                                binding_components[binding_id].append(tmp)
+                                binding_components[binding_id].append(en_comp)
+
 
         except IndexError:
             quit()
@@ -332,17 +365,22 @@ def read_patterns(m, p, species):
 
     s_list = []
     
+
     if 'type' in species and species['type'] == 'add':
         for entity in species['children']:
             sp = read_species(m, p, entity)
-            sp = m.register_species(sp)
+            sp.concrete = False
+            sp = m.register_species(sp)  # koko de m.concrete_species ni hairu
+#            m.concrete_species.clear()   # syouganai node kesu
             s_list.append(sp)
 
     else:
         sp = read_species(m, p, species)
-        sp = m.register_species(sp)
+        sp.concrete = False
+        sp = m.register_species(sp)  # koko de m.concrete_species ni hairu
+#        m.concrete_species.clear()   # syouganai node kesu
+        sp.concrete = False
         s_list.append(sp)
-
 
     return s_list
 
