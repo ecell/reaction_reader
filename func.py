2827abcf3ae00ee1186eca32bedb3c1ec84ce4a2
Kozo Nishida <knishida@riken.jp>
2011-08-09 13:44:53 +0900

    added reversible MassAction

--- func.py
+++ func.py
@@ -1,12 +1,18 @@
 from model.model import *
+#from model.model import IncludingEntityCondition
 
 def michaelis_menten(a=None):
+#    print '*** michaelis_menten(',a,') is called ***'
     return float(a)
 
 def MassAction(a=None):
     return float(a)
 
+def MassAction2(a=None, b=None):
+    return a, b
+
 def include_reactants(a=None, b=None):
+#    print '*** include_reactants(', a, ',', b._key, ') is called ***'
     d = EntityType(b._key)
     d.__name=b._key
     c = IncludingEntityCondition(REACTANTS, int(a), d)
