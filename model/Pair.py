b944b21d15f64933f7e8eca67ece3696181b9551
Kozo Nishida <knishida@riken.jp>
2012-01-05 10:11:26 +0900

    added product_entity

--- model/Pair.py
+++ model/Pair.py
@@ -19,6 +19,7 @@ class Pair(object):
         '''
         raise Error('Not implemented.')
 
+
 class PatternMatchingEntityPair(Pair):
     '''
     A pair of entities.
@@ -91,86 +92,6 @@ class PatternMatchingEntityPair(Pair):
         retval += '%s(%d)' % (e_2.name, e_2.id)
         return retval
 
-class ReactantSpeciesEntityPair(Pair):
-    '''
-    A pair of entities.
-    One is in a reactant pattern species, and the other is in a concrete 
-    species.
-    '''
-
-    def __init__(self, reactant_index, reactant_entity, \
-        species_entity):
-        '''
-        Initializes this pair object.
-
-        reactant_index: The reactant index.
-        reactant_entity: The reactant entity.
-        species_entity: The entity of the concrete species.
-        '''
-        self.__reactant_index = reactant_index
-        self.__reactant_entity = reactant_entity
-        self.__species_entity = species_entity
-
-    @property
-    def reactant_index(self):
-        '''Returns the reactant index.'''
-        return self.__reactant_index
-
-    @property
-    def reactant_entity(self):
-        '''Returns the reactant entity.'''
-        return self.__reactant_entity
-
-    @property
-    def species_entity(self):
-        '''Returns the entity of the concrete species.'''
-        return self.__species_entity
-
-    def has_equal_first_element(self, pair):
-        '''
-        Returns whether this pair has the same reactant index
-        and reantant entity with the given pair.
-
-        pair: An entity pair of reactant pattern and a concrete species.
-        '''
-        return self.reactant_index == pair.reactant_index \
-            and self.reactant_entity == pair.reactant_entity
-
-    def has_equal_second_element(self, pair):
-        '''
-        Returns whether this pair has the same product entity 
-        with the given pair.
-
-        pair: An entity pair of reactant pattern and a concrete species.
-        '''
-        return self.species_entity == pair.species_entity
-
-    def __str__(self):
-        '''Returns the string representation of this object.'''
-        retval = '%s(%d,%d)' % (self.reactant_entity.name, \
-            self.reactant_index, self.reactant_entity.id)
-        retval += '-'
-        retval += '%s(%d)' % (self.species_entity.name, \
-            self.species_entity.id)
-        return retval
-
-    def __cmp__(self, other):
-        '''
-        Compares this object with a given other pair and returns the result.
-
-        other: A pair.
-        '''
-        if self.reactant_index < other.reactant_index:
-            return -1
-        elif self.reactant_index > other.reactant_index:
-            return 1
-        else:
-            if self.reactant_entity.id < other.reactant_entity.id:
-                return -1
-            elif self.reactant_entity.id > other.reactant_entity.id:
-                return 1
-            else:
-                return 0
 
 class ReactantProductEntityPair(Pair):
     '''
@@ -270,6 +191,88 @@ class ReactantProductEntityPair(Pair):
             if self.product_entity.id < other.product_entity.id:
                 return -1
             elif self.product_entity.id > other.product_entity.id:
+                return 1
+            else:
+                return 0
+
+
+class ReactantSpeciesEntityPair(Pair):
+    '''
+    A pair of entities.
+    One is in a reactant pattern species, and the other is in a concrete 
+    species.
+    '''
+
+    def __init__(self, reactant_index, reactant_entity, \
+        species_entity):
+        '''
+        Initializes this pair object.
+
+        reactant_index: The reactant index.
+        reactant_entity: The reactant entity.
+        species_entity: The entity of the concrete species.
+        '''
+        self.__reactant_index = reactant_index
+        self.__reactant_entity = reactant_entity
+        self.__species_entity = species_entity
+
+    @property
+    def reactant_index(self):
+        '''Returns the reactant index.'''
+        return self.__reactant_index
+
+    @property
+    def reactant_entity(self):
+        '''Returns the reactant entity.'''
+        return self.__reactant_entity
+
+    @property
+    def species_entity(self):
+        '''Returns the entity of the concrete species.'''
+        return self.__species_entity
+
+    def has_equal_first_element(self, pair):
+        '''
+        Returns whether this pair has the same reactant index
+        and reantant entity with the given pair.
+
+        pair: An entity pair of reactant pattern and a concrete species.
+        '''
+        return self.reactant_index == pair.reactant_index \
+            and self.reactant_entity == pair.reactant_entity
+
+    def has_equal_second_element(self, pair):
+        '''
+        Returns whether this pair has the same product entity 
+        with the given pair.
+
+        pair: An entity pair of reactant pattern and a concrete species.
+        '''
+        return self.species_entity == pair.species_entity
+
+    def __str__(self):
+        '''Returns the string representation of this object.'''
+        retval = '%s(%d,%d)' % (self.reactant_entity.name, \
+            self.reactant_index, self.reactant_entity.id)
+        retval += '-'
+        retval += '%s(%d)' % (self.species_entity.name, \
+            self.species_entity.id)
+        return retval
+
+    def __cmp__(self, other):
+        '''
+        Compares this object with a given other pair and returns the result.
+
+        other: A pair.
+        '''
+        if self.reactant_index < other.reactant_index:
+            return -1
+        elif self.reactant_index > other.reactant_index:
+            return 1
+        else:
+            if self.reactant_entity.id < other.reactant_entity.id:
+                return -1
+            elif self.reactant_entity.id > other.reactant_entity.id:
                 return 1
             else:
                 return 0
