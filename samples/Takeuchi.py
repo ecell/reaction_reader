5ce39113c16785c6e5ce235e2cabe7d65bb1d8a1
Kozo Nishida <knishida@riken.jp>
2012-05-09 11:27:25 +0900

    just uncomment

--- samples/Takeuchi.py
+++ samples/Takeuchi.py
@@ -1,24 +1,25 @@
 with molecule_types:
-#    A(a1 = (U, A1), a2 = (U, A2), a3 = (U, A3))
-#    B(b = (a, b, 0, 1))
-#    C(C)
-#    D(d, d1 = (0, 1), d2 = (0, 1), d3 = (0, 1))
-#    E(e)
+    A(a1 = (U, A1), a2 = (U, A2), a3 = (U, A3))
+    B(b = (a, b, 0, 1))
+    C(C)
+    D(d, d1 = (0, 1), d2 = (0, 1), d3 = (0, 1))
+    E(e)
 
     egf(r)
     egfr(l, d, Y = (U, P))
 
 with molecule_inits:
-#    E(e) [500]
-#    D(d, d1 = 0, d2 = 0, d3 = 1) [420]
-#    D(d, d1 = 1, d2 = 1, d3 = 0) [440]
-#    A(a1 = U, a3 = A3, a2 = U) [300]
-#    C(C) [200]
-#    B(b = a) [610]
-#    B(b = b) [620]
-#    B(b = 0) [630]
-#    B(b = 1) [640]
+    E(e) [500]
+    D(d, d1 = 0, d2 = 0, d3 = 1) [420]
+    D(d, d1 = 1, d2 = 1, d3 = 0) [440]
+    A(a1 = U, a3 = A3, a2 = U) [300]
+    C(C) [200]
+    B(b = a) [610]
+    B(b = b) [620]
+    B(b = 0) [630]
+    B(b = 1) [640]
 
+    egf(r).egfr(l = 1) [160]
     egf(r) [160]
     egf(r[1]) [160]
     egfr(l, d[1], X = A, Y = P[1], XX=Axs[3], YY=True, a=a) [140]
@@ -27,6 +28,20 @@ with molecule_inits:
     egf(r[3]).egfr(l[1], d[2], Y = U).egf(r[2]).egf(r) [100]
 
 with reaction_rules:
+    A(a) > B(b) [C(c)]            | 2.1 # [E]
+    A(a) > B(b) [C(c).C(c)]       | 2.2 # [ES]
+    A(a) > B(b) [C(c), C(c)]      | 2.3 # [E, E]
+    A(a) > B(b) [C(c).C(c), C(c)] | 2.4 # [ES, E]
+    A(a) > B(b) [C(c), C(c).C(c)] | 2.5 # [E, ES]
+
+    A(a) > B(b)             [C(c)] | 1.1 # E [effector]
+    A(a) > B(b).B(b)        [C(c)] | 1.2 # ES [effector]
+    A(a) > B(b) + B(b).B(b) [C(c)] | 1.3 # ESL [effector]
+    
+    A(a)             + B(b).C(c) > D(d) | 0.1 # E + ES
+    A(a).A(a)        + B(b).C(c) > D(d) | 0.2 # ES + ES
+    A(a) + A(a).A(a) + B(b).C(c) > D(d) | 0.3 # ESL + ES
+
     egfr(l, d[1], X = A, Y = P[1], XX=Axs[3], YY=True, a=a).eg(a) > A(a) | 0.1
     A(a[1]) > G(g) | 0.1
 
