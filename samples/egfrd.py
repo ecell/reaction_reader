fc55c484869e423d73f1a9695dea45315136c91d
knishida <knishida@knishida-Precision-WorkStation-T3500.(none)>
2012-07-09 18:44:15 +0900

    * samples/egfrd.py: use _ for BINDING_ANY and stop using <_> for reversible temporarily

--- samples/egfrd.py
+++ samples/egfrd.py
@@ -17,10 +17,10 @@ with molecule_inits:
 
 with reaction_rules:
     # Ligand-receptor binding
-    egfr(l,r) + egf(r) <_> egfr(l[1],r).egf(r[1]) | (1.667e-06, .06)
+    #egfr(l,r) + egf(r) <_> egfr(l[1],r).egf(r[1]) | (1.667e-06, .06)
 
     # Receptor-aggregation
-    egfr(l[1],r) + egfr(l[2],r) <_> egfr(l[1],r[3]).egfr(l[2],r[3]) | (5.556e-06, .1)
+    #egfr(l[1],r) + egfr(l[2],r) <_> egfr(l[1],r[3]).egfr(l[2],r[3]) | (5.556e-06, .1)
 
     # Transphosphorylation of egfr by RTK
     egfr(r,Y1068 = (Y)) > egfr(r,Y1068 = (pY)) | .5
@@ -31,34 +31,34 @@ with reaction_rules:
     egfr(Y1148 = (pY)) > egfr(Y1148 = (Y)) | 4.505
 
     # Shc transphosph
-    egfr(r[2], Y1148 = (pY)[1]).Shc(PTB[1], Y317 = (Y)) > egfr(r[2], Y1148 = (pY)[1]).Shc(PTB[1], Y317 = (pY)) | 3.0
-    Shc(PTB[1], Y317 = (pY)) > Shc(PTB[1], Y317 = (Y)) | .03
+    egfr(r[_], Y1148 = (pY)[1]).Shc(PTB[1], Y317 = (Y)) > egfr(r[_], Y1148 = (pY)[1]).Shc(PTB[1], Y317 = (pY)) | 3.0
+    Shc(PTB[_], Y317 = (pY)) > Shc(PTB[_], Y317 = (Y)) | .03
 
     # Y1068 activity
-    egfr(Y1068 = (pY)) + Grb2(SH2, SH3) <_> egfr(Y1068 = (pY)[1]).Grb2(SH2[1], SH3) | (8.333e-07, .05)
-    egfr(Y1068 = (pY)) + Grb2(SH2, SH3[2]) <_> egfr(Y1068 = (pY)[1]).Grb2(SH2[1], SH3[2]) | (1.25e-06, .03)
-    egfr(Y1068 = (pY)[1]).Grb2(SH2[1], SH3) + Sos(dom) <_> egfr(Y1068 = (pY)[1]).Grb2(SH2[1],SH3[2]).Sos(dom[2]) | (5.556e-06, .06)
+    # egfr(Y1068 = (pY)) + Grb2(SH2, SH3) <_> egfr(Y1068 = (pY)[1]).Grb2(SH2[1], SH3) | (8.333e-07, .05)
+    # egfr(Y1068 = (pY)) + Grb2(SH2, SH3[2]) <_> egfr(Y1068 = (pY)[1]).Grb2(SH2[1], SH3[2]) | (1.25e-06, .03)
+    # egfr(Y1068 = (pY)[1]).Grb2(SH2[1], SH3) + Sos(dom) <_> egfr(Y1068 = (pY)[1]).Grb2(SH2[1],SH3[2]).Sos(dom[2]) | (5.556e-06, .06)
 
     # Y1148 activity
-    egfr(Y1148 = (pY)) + Shc(PTB, Y317 = (Y)) <_> egfr(Y1148 = (pY)[1]).Shc(PTB[1], Y317 = (Y)) | (2.5e-05, .6)
-    egfr(Y1148 = (pY)) + Shc(PTB, Y317 = (pY)) <_> egfr(Y1148 = (pY)[1]).Shc(PTB[1],Y317 = (pY)) | (2.5e-07, .3)
-    egfr(Y1148 = (pY)) + Shc(PTB, Y317 = (pY)[1]).Grb2(SH2[1], SH3) <_> \
-           egfr(Y1148 = (pY)[2]).Shc(PTB[2], Y317 = (pY)[1]).Grb2(SH2[1], SH3) | (2.5e-07, .3)
-    egfr(Y1148 = (pY)) + Shc(PTB, Y317 = (pY)[1]).Grb2(SH2[1], SH3[3]).Sos(dom[3]) <_> \
-           egfr(Y1148 = (pY)[2]).Shc(PTB[2], Y317 = (pY)[1]).Grb2(SH2[1],SH3[3]).Sos(dom[3]) | (6.667e-08, .12)
+    # egfr(Y1148 = (pY)) + Shc(PTB, Y317 = (Y)) <_> egfr(Y1148 = (pY)[1]).Shc(PTB[1], Y317 = (Y)) | (2.5e-05, .6)
+    # egfr(Y1148 = (pY)) + Shc(PTB, Y317 = (pY)) <_> egfr(Y1148 = (pY)[1]).Shc(PTB[1],Y317 = (pY)) | (2.5e-07, .3)
+    # egfr(Y1148 = (pY)) + Shc(PTB, Y317 = (pY)[1]).Grb2(SH2[1], SH3) <_> \
+    #        egfr(Y1148 = (pY)[2]).Shc(PTB[2], Y317 = (pY)[1]).Grb2(SH2[1], SH3) | (2.5e-07, .3)
+    # egfr(Y1148 = (pY)) + Shc(PTB, Y317 = (pY)[1]).Grb2(SH2[1], SH3[3]).Sos(dom[3]) <_> \
+    #        egfr(Y1148 = (pY)[2]).Shc(PTB[2], Y317 = (pY)[1]).Grb2(SH2[1],SH3[3]).Sos(dom[3]) | (6.667e-08, .12)
 
-    egfr(Y1148 = (pY)[1]).Shc(PTB[1], Y317 = (pY)) + Grb2(SH2, SH3) <_> \
-           egfr(Y1148 = (pY)[1]).Shc(PTB[1], Y317 = (pY)[2]).Grb2(SH2[2], SH3) | (1.667e-06, .1)
+    # egfr(Y1148 = (pY)[1]).Shc(PTB[1], Y317 = (pY)) + Grb2(SH2, SH3) <_> \
+    #        egfr(Y1148 = (pY)[1]).Shc(PTB[1], Y317 = (pY)[2]).Grb2(SH2[2], SH3) | (1.667e-06, .1)
 
-    egfr(Y1148 = (pY)[1]).Shc(PTB[1],Y317 = (pY)) + Grb2(SH2, SH3[3]).Sos(dom[3]) <_> \
-           egfr(Y1148 = (pY)[1]).Shc(PTB[1], Y317 = (pY)[2]).Grb2(SH2[2], SH3[3]).Sos(dom[3]) | (5e-06, .0429)
+    # egfr(Y1148 = (pY)[1]).Shc(PTB[1],Y317 = (pY)) + Grb2(SH2, SH3[3]).Sos(dom[3]) <_> \
+    #        egfr(Y1148 = (pY)[1]).Shc(PTB[1], Y317 = (pY)[2]).Grb2(SH2[2], SH3[3]).Sos(dom[3]) | (5e-06, .0429)
 
-    Shc(PTB[1], Y317 = (pY)[2]).Grb2(SH2[2], SH3) + Sos(dom) <_> \
-           Shc(PTB[1], Y317 = (pY)[2]).Grb2(SH2[2], SH3[3]).Sos(dom[3]) | (5.556e-06, 0.0214)
+    # Shc(PTB[1], Y317 = (pY)[2]).Grb2(SH2[2], SH3) + Sos(dom) <_> \
+    #        Shc(PTB[1], Y317 = (pY)[2]).Grb2(SH2[2], SH3[3]).Sos(dom[3]) | (5.556e-06, 0.0214)
 
-    # Cytosolic
-    Shc(PTB, Y317 = (pY)) + Grb2(SH2, SH3)    <_> Shc(PTB, Y317 = (pY)[1]).Grb2(SH2[1], SH3)    | (1.667e-06, .01)
-    Shc(PTB, Y317 = (pY)) + Grb2(SH2, SH3[2]) <_> Shc(PTB, Y317 = (pY)[1]).Grb2(SH2[1], SH3[2]) | (1.167e-05, .1)
+    # # Cytosolic
+    # Shc(PTB, Y317 = (pY)) + Grb2(SH2, SH3)    <_> Shc(PTB, Y317 = (pY)[1]).Grb2(SH2[1], SH3)    | (1.667e-06, .01)
+    # Shc(PTB, Y317 = (pY)) + Grb2(SH2, SH3[2]) <_> Shc(PTB, Y317 = (pY)[1]).Grb2(SH2[1], SH3[2]) | (1.167e-05, .1)
     Shc(PTB, Y317 = (pY)) > Shc(PTB, Y317 = (Y)) | .005
-    Grb2(SH2, SH3) + Sos(dom) <_> Grb2(SH2, SH3[1]).Sos(dom[1]) | (5.556e-08, .0015)
-    Shc(PTB, Y317 = (pY)[2]).Grb2(SH2[2], SH3) + Sos(dom) <_> Shc(PTB, Y317 = (pY)[2]).Grb2(SH2[2], SH3[3]).Sos(dom[3]) | (1.667e-05, .064)
+    # Grb2(SH2, SH3) + Sos(dom) <_> Grb2(SH2, SH3[1]).Sos(dom[1]) | (5.556e-08, .0015)
+    # Shc(PTB, Y317 = (pY)[2]).Grb2(SH2[2], SH3) + Sos(dom) <_> Shc(PTB, Y317 = (pY)[2]).Grb2(SH2[2], SH3[3]).Sos(dom[3]) | (1.667e-05, .064)
