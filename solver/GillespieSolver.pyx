de9120eca2b75b773a0327fc406c1ad39c723150
YukiSakamoto <j.c.ferrero.esp@gmail.com>
2012-08-06 13:38:29 +0900

    Merge GillespieSolver

---
+++ solver/GillespieSolver.pyx
@@ -0,0 +1,59 @@
+import numpy as np
+cimport numpy as np 
+
+cdef extern from "Gillespie.hpp":
+    ctypedef struct c_Solver "GillespieSolver":
+        double step()
+        double duration(double)
+        void set_current_time(double)
+        double get_current_time()
+        int  get_current_state(int*,int)
+        void set_current_state(int*,int)
+        int  reaction_add()
+        void reaction_add_substance(int,int,int)
+        void reaction_add_product(int,int,int)
+        void reaction_set_kinetic_parameter(int,double)
+    c_Solver *new_solver "new GillespieSolver"()
+    void delete_solver "delete" (c_Solver*)
+
+cdef class GillespieSolver:
+    cdef c_Solver *thisptr
+    def __cinit__(self):
+        self.thisptr = new_solver()
+
+    def __del__(self):
+        delete_solver(self.thisptr)
+
+    def step(self):
+        return self.thisptr.step()
+
+    def duration(self,dt):
+        return self.thisptr.duration(dt)
+
+    def get_current_time(self):
+        return self.thisptr.get_current_time()
+
+    def set_current_time(self,new_t):
+        self.thisptr.set_current_time(new_t)
+
+    def reaction_add(self):
+        return self.thisptr.reaction_add()
+
+    def reaction_add_substance(self,react_id,specie_index, stoichiometry):
+        self.thisptr.reaction_add_substance(react_id,specie_index,stoichiometry)
+
+    def reaction_add_product(self,react_id,specie_index,stoichiometry):
+        self.thisptr.reaction_add_product(react_id,specie_index,stoichiometry)
+
+    def reaction_set_kinetic_parameter(self,react_id,k):
+        self.thisptr.reaction_set_kinetic_parameter(react_id,k)
+
+    def set_current_state(self, np.ndarray py_array):
+        cdef int *int_array = <int *>py_array.data
+        cdef int length = len(py_array)
+        self.thisptr.set_current_state(int_array, length)
+
+    def get_current_state(self, np.ndarray py_array):
+        cdef int *int_array = <int *>py_array.data 
+        cdef int length = len(py_array)
+        self.thisptr.get_current_state(int_array, length)
--- solver/setup.py
+++ solver/setup.py
@@ -1,12 +1,16 @@
 IncludeDirListODE = ["cpp_lib/ODE"]
 IncludeDirListDAE = ["cpp_lib/DAE"]
+IncludeDirListGillespie = ["cpp_lib/Gillespie"]
+#numpy_include = ['/System/Library/Frameworks/Python.framework/Versions/Current/Extras/lib/python/numpy/core/include/']
 
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+
 sourcefilesODE = ['ODESolver.pyx', 'cpp_lib/ODE/ODESolver.cpp']
 sourcefilesDAE = ['DAESolver.pyx', 'cpp_lib/DAE/DAESolver.cpp']
+sourcefilesGillespie = ['GillespieSolver.pyx', 'cpp_lib/Gillespie/Gillespie.cpp']
 
 setup(
     cmdclass = dict(build_ext = build_ext),
@@ -15,7 +19,7 @@ setup(
             'ODESolver',
             sourcefilesODE,
             language='c++',
-            include_dirs = IncludeDirListODE,
+            include_dirs = IncludeDirListODE + numpy_include,
             libraries = ['gsl', 'gslcblas'],
             library_dirs = ['/usr/lib']
             )
@@ -29,10 +33,24 @@ setup(
             'DAESolver',
             sourcefilesDAE,
             language='c++',
-            include_dirs = IncludeDirListDAE,
+            include_dirs = IncludeDirListDAE + numpy_include,
             libraries = ['gsl', 'gslcblas'],
             library_dirs = ['/usr/lib']
             )
         ]
     )
 
+
+import numpy
+setup(
+    cmdclass = dict(build_ext = build_ext),
+    ext_modules = [
+        Extension(
+            'GillespieSolver',
+            sourcefilesGillespie,
+            include_dirs = IncludeDirListGillespie + numpy_include,
+            language='c++',
+            libraries = ['gsl', 'gslcblas'],
+            )
+        ]
+    )
